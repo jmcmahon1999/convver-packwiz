718bb546e78bb556e0fc30c60b57433042652235
Joe McMahon <jmcmahon1999@gmail.com>
2025-03-24 23:50:18 +0000

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const TOML = require('smol-toml');
+const convver = require('convver');
+
+module.exports = {
+  name: "packwiz",
+  description: "plugin for packwiz projects",
+  file: "pack.toml",
+  async read() {
+    const data = fs.readFileSync(path.resolve(this.file));
+    if (data) {
+        return TOML.parse(data.toString())
+    } else {
+        return;
+    }
+  },
+  async version() {
+    const project = await this.read();
+    return convver.api.verifyVersion(project.version);
+  },
+  async update(version) {
+    const project = await this.read();
+    project.version = version;
+    fs.writeFileSync(path.resolve(this.file), TOML.stringify(project));
+  },
+};
